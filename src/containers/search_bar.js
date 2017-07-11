d94cab34418bb022cc0bf8e7483faed1cba9b53b
Jacob Parma <jacobparma@gmail.com>
2017-07-11 14:32:14 -0500

    S5-54 Form Elements

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -13,9 +13,15 @@ export default class SearchBar extends Component {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // Go and fetch requested weather data
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form className="input-group" onSubmit={this.onFormSubmit}>
         <input 
           placeholder="Get a five day forecast in your favorite cities"
           className="form-control"

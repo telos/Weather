29e2a60918d3a300281025060f031d74c563d346
Jacob Parma <jacobparma@gmail.com>
2017-07-12 11:44:40 -0500

    S5-60 Avoiding State Mutations in Reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,8 +1,21 @@
+import { FETCH_WEATHER } from '../actions/index';
 
-export default function(state = null, action) {
+export default function(state = [], action) {
   // redux-promise middleware will have resolved the promise so the action we recieve
   // will contain the resolved promise with data
   // console.log('Action recieved', action);
+
+  switch(action.type) {
+    case FETCH_WEATHER:
+      // Do not return the below as it will mutate state
+      // return state.push(action.payload.data);
+
+      // Need to return a new copy of state with the old and new data combined
+      // return state.concat([action.payload.data]);
+
+      // Or use the ES6 shorthand below to do the same thing and concatenate the arrays
+      return [ action.payload.data, ...state ];
+  }
 
   return state;
 }
